86859578db740d56bb29dd60b5df238b4df89e93
Willians Faria <williansfaria@hotmail.com>
2020-08-08 17:12:34 -0300

    refactor: refactoring create app class for a more SOLID code

--- src/UseCases/CreateApp/CreateAppUseCase.spec.ts
+++ src/UseCases/CreateApp/CreateAppUseCase.spec.ts
@@ -1,15 +1,18 @@
 import { CreateAppUseCase } from './CreateAppUseCase'
 import { MessagesRepository } from '../../repositories/Implementations/MessagesRepository'
+import { FileManagerRepository } from '../../repositories/Implementations/FileManagerRepository'
 
 describe('CreateAppUseCase', () => {
 	test('Expect createAppUseCase to be defined', () => {
 		const messagesRepository = new MessagesRepository()
-		const createAppUseCase = new CreateAppUseCase(messagesRepository)
+		const fileManager = new FileManagerRepository()
+		const createAppUseCase = new CreateAppUseCase(messagesRepository, fileManager)
 		expect(createAppUseCase).toBeDefined()
 	})
 	test('Expect createAppUseCase to be instance of CreateAppUseCase', () => {
 		const messagesRepository = new MessagesRepository()
-		const createAppUseCase = new CreateAppUseCase(messagesRepository)
+		const fileManager = new FileManagerRepository()
+		const createAppUseCase = new CreateAppUseCase(messagesRepository, fileManager)
 		expect(createAppUseCase).toBeInstanceOf(CreateAppUseCase)
 	})
 })
