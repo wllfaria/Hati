7f6f593ac56ab66c1dcf456b11e6a4d7e1a7d803
Willians Faria <williansfaria@hotmail.com>
2020-08-13 19:38:24 -0300

    feat: unit tests for database

--- templates/typescript/PBF/sequelize-template/src/Database.spec.ts
+++ templates/typescript/PBF/sequelize-template/src/Database.spec.ts
@@ -5,4 +5,26 @@ describe('Database', () => {
 		const database = new Database()
 		expect(database).toBeDefined()
 	})
+	test('Expect database.getInstance to be defined', () => {
+		expect(Database.getInstance).toBeDefined()
+	})
+	test('Expect database.getInstance to be a function', () => {
+		expect(Database.getInstance).toBeInstanceOf(Function)
+	})
+	test('Expect database.getInstance to return a Database', () => {
+		const database = Database.getInstance()
+		expect(database).toBeInstanceOf(Database)
+	})
+	test('Expect database.truncate to be defined', () => {
+		expect(Database.truncate).toBeDefined()
+	})
+	test('Expect database.truncate to be a function', () => {
+		expect(Database.truncate).toBeInstanceOf(Function)
+	})
+	test('Expect database.getTransaction to be defined', () => {
+		expect(Database.getTransaction).toBeDefined()
+	})
+	test('Expect database.getTransaction to be a function', () => {
+		expect(Database.getTransaction).toBeInstanceOf(Function)
+	})
 })
